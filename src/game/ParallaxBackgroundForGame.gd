d93af48a11a0ac62c5d2b17a87f995fd5a589087
KingFuGitHub <kingfu321@gmail.com>
2021-11-30 14:43:42 -0800

    Long's Commit
    Animated background should loop infinitely. Lowered the player's ship death sound effect.

--- src/game/ParallaxBackgroundForGame.gd
+++ src/game/ParallaxBackgroundForGame.gd
@@ -15,28 +15,31 @@ onready var texture9 = $"ParallaxLayer/TextureRect9"
 
 func _process(delta):
 	scroll_base_offset.y += scrolling_speed * delta
-	if(GameManager.level == 2):
+	if(GameManager.level%9 == 1):
+		texture1.visible = true
+		texture9.visible = false
+	elif(GameManager.level%9 == 2):
 		texture2.visible = true
 		texture1.visible = false
-	elif(GameManager.level == 3):
+	elif(GameManager.level%9 == 3):
 		texture3.visible = true
 		texture2.visible = false
-	elif(GameManager.level == 4):
+	elif(GameManager.level%9 == 4):
 		texture4.visible = true
 		texture3.visible = false
-	elif(GameManager.level == 5):
+	elif(GameManager.level%9 == 5):
 		texture5.visible = true
 		texture4.visible = false
-	elif(GameManager.level == 6):
+	elif(GameManager.level%9 == 6):
 		texture6.visible = true
 		texture5.visible = false
-	elif(GameManager.level == 7):
+	elif(GameManager.level%9 == 7):
 		texture7.visible = true
 		texture6.visible = false
-	elif(GameManager.level == 8):
+	elif(GameManager.level%9 == 8):
 		texture8.visible = true
 		texture7.visible = false
-	elif(GameManager.level == 9):
+	elif(GameManager.level%9 == 0):
 		texture9.visible = true
 		texture8.visible = false
 	
--- src/game/player/explosion/Explosion.gd
+++ src/game/player/explosion/Explosion.gd
@@ -12,14 +12,13 @@ var index = 0
 func _ready():
 	self.add_child(deathSoundEffect)
 	deathSoundEffect.stream = load("res://assets/SoundEffect/death2.wav")
-	deathSoundEffect.volume_db = -20
 	play_animation()
 
 func _on_Explosion_animation_finished():
 	play_animation()
 
 func play_animation():
-	deathSoundEffect.volume_db = -10
+	deathSoundEffect.volume_db = -20
 	deathSoundEffect.play()
 	if index < animations.size():
 		animated_sprite.play(animations[index])
